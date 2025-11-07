e4bc43c64ed9713dacd1bf506c8eb723f4ab295a
Beyza <u.beyza.simsek@gmail.com>
2025-11-07 18:38:55 +0300

    Home Screen and resumeCard

--- NotesToMyself.md
+++ NotesToMyself.md
@@ -1,3 +1,7 @@
 # REACT NOTES
 
 - **app > routes.ts:** React Routers configuration file.
+
+# CSS NOTES
+
+- **_inset-0_** -> top-0 left-0 right-0 bottom-0
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -18,20 +18,21 @@ export default function Home() {
     
     {/* Hero */}
     <section className="main-section">
-      <div className="page-heading">
+      <div className="page-heading py-16">
         <h1>Track Your Applications & Resume Ratings</h1>
         <h2>Review your submissions and check AI-powered feedback.</h2>
       </div>
-    </section>
+    
 
-    {/* Resume Cards */}
-    {resumes.length > 0 && (
-      <div className="resumes-section">
-      {resumes.map( (resume) => (
-        <ResumeCard key={resume.id} resume={resume} />
-      ) )}
-    </div>
-    )}
+      {/* Resume Cards */}
+      {resumes.length > 0 && (
+        <div className="resumes-section">
+        {resumes.map( (resume) => (
+          <ResumeCard key={resume.id} resume={resume} />
+        ) )}
+      </div>
+      )}
+    </section>
     
 
   </main>
