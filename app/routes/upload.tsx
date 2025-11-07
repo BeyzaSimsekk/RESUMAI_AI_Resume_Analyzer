349101ef57758b25cc8dfef63623e593cf7088ab
Beyza <u.beyza.simsek@gmail.com>
2025-11-07 22:10:28 +0300

    upload.tsx done

--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -1,13 +1,36 @@
 import { useState, type FormEvent } from 'react';
+import FileUploader from '~/components/FileUploader';
 import Navbar from '~/components/Navbar'
 
 const Upload = () => {
 
   const [isProcessing, setIsProcessing] = useState(false);
   const [statusText, setStatusText] = useState('');
+  const [file, setFile] = useState<File | null>(null);
 
 
-  const handleSubmit = (e : FormEvent<HTMLFormElement>) => {}
+  const handleFileSelect = (file: File | null) => {
+    setFile(file);
+  }
+
+  const handleSubmit = (e : FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const form = e.currentTarget.closest('form');
+    if(!form) return;
+    const formData = new FormData(form);
+
+    const companyName = formData.get('company-name');
+    const jobTitle = formData.get('job-title');
+    const jobDescription = formData.get('job-description');
+
+    console.log({
+      companyName,
+      jobTitle,
+      jobDescription,
+      file
+    })
+
+  }
 
 
   return (
@@ -43,7 +66,7 @@ const Upload = () => {
               </div>
               <div className='form-div'>
                 <label htmlFor="uploader">Upload Resume</label>
-                <div>Uploader</div>
+                <FileUploader onFileSelect={handleFileSelect}/>
               </div>
 
               <button className='primary-button' type='submit'>Analyze Resume</button>
